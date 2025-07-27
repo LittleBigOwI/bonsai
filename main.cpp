837db404558e415d31b51e47cda061907edfae2c
LittleBigOwI <syl20.row@gmail.com>
2025-07-27 19:53:32 +0200

    Reformat code

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 
+#define SIDEBAR_WIDTH 50
+
 int main() {
     auto sidebar_renderer = ftxui::Renderer([&] {
         return ftxui::vbox({
             ftxui::text("Hello World") | ftxui::bold | ftxui::center,
-        }) | ftxui::border | size(ftxui::WIDTH, ftxui::EQUAL, 30); // Fixed width sidebar
+        }) | ftxui::border | size(ftxui::WIDTH, ftxui::EQUAL, SIDEBAR_WIDTH);
     });
 
     auto right_pane = ftxui::Renderer([&] {
