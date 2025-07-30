52f47254c90123fc41c2f38f3fb0de0e0380e27c
LittleBigOwI <syl20.row@gmail.com>
2025-07-30 21:03:05 +0200

    Add extra angled ellipse methods

--- widgets/canvas_ext.cpp
+++ widgets/canvas_ext.cpp
@@ -2,15 +2,21 @@
 
 #include <cmath>
 
-constexpr auto nostyle = [](ftxui::Pixel&) {};
+constexpr auto nostyle = [](ftxui::Pixel &) {};
 
-void BonsaiCanvas::DrawAngledBlockEllipse(int x1, int y1, int r1, int r2,
-                                          double max_angle_deg) {
+void BonsaiCanvas::DrawAngledBlockEllipse(int x1, int y1, int r1, int r2, double max_angle_deg)
+{
     this->DrawAngledBlockEllipse(x1, y1, r1, r2, nostyle, max_angle_deg);
 }
 
-void BonsaiCanvas::DrawAngledBlockEllipse(int x1, int y1, int r1, int r2,
-                                          const Stylizer& s, double max_angle_deg) {
+void BonsaiCanvas::DrawAngledBlockEllipse(int x, int y, int r1, int r2, const ftxui::Color &color, double max_angle_deg)
+{
+    this->DrawAngledBlockEllipse(x, y, r1, r2, [color](ftxui::Pixel &p)
+                                 { p.foreground_color = color; }, max_angle_deg);
+}
+
+void BonsaiCanvas::DrawAngledBlockEllipse(int x1, int y1, int r1, int r2, const Stylizer &s, double max_angle_deg)
+{
     y1 /= 2;
     r2 /= 2;
     int x = -r1;
@@ -22,44 +28,56 @@ void BonsaiCanvas::DrawAngledBlockEllipse(int x1, int y1, int r1, int r2,
 
     double max_angle_rad = max_angle_deg * M_PI / 180.0;
 
-    auto draw_if_within_angle = [&](int dx, int dy) {
+    auto draw_if_within_angle = [&](int dx, int dy)
+    {
         double angle = atan2(dy * r1, dx * r2);
         if (angle < 0)
-        angle += 2 * M_PI;
+            angle += 2 * M_PI;
         if (angle <= max_angle_rad)
-        this->DrawBlock(x1 + dx, 2 * (y1 + dy), true, s);
+            this->DrawBlock(x1 + dx, 2 * (y1 + dy), true, s);
     };
 
-    do {
+    do
+    {
         draw_if_within_angle(-x, y);
         draw_if_within_angle(x, y);
         draw_if_within_angle(x, -y);
         draw_if_within_angle(-x, -y);
 
         e2 = 2 * err;
-        if (e2 >= dx) {
-        x++;
-        err += dx += 2 * r2 * r2;
+        if (e2 >= dx)
+        {
+            x++;
+            err += dx += 2 * r2 * r2;
         }
-        if (e2 <= dy) {
-        y++;
-        err += dy += 2 * r1 * r1;
+        if (e2 <= dy)
+        {
+            y++;
+            err += dy += 2 * r1 * r1;
         }
     } while (x <= 0);
 
-    while (y++ < r2) {
+    while (y++ < r2)
+    {
         draw_if_within_angle(0, y);
         draw_if_within_angle(0, -y);
     }
 }
 
-void BonsaiCanvas::DrawAngledPointEllipse(int x1, int y1, int r1, int r2,
-                                          double max_angle_deg) {
+void BonsaiCanvas::DrawAngledPointEllipse(int x1, int y1, int r1, int r2, double max_angle_deg)
+{
     this->DrawAngledPointEllipse(x1, y1, r1, r2, nostyle, max_angle_deg);
 }
 
+void BonsaiCanvas::DrawAngledPointEllipse(int x, int y, int r1, int r2, const ftxui::Color &color, double max_angle_deg)
+{
+    this->DrawAngledPointEllipse(x, y, r1, r2, [color](ftxui::Pixel &p)
+                                 { p.foreground_color = color; }, max_angle_deg);
+}
+
 void BonsaiCanvas::DrawAngledPointEllipse(int x1, int y1, int r1, int r2,
-                                          const Stylizer& s, double max_angle_deg) {
+                                          const Stylizer &s, double max_angle_deg)
+{
     y1 /= 2;
     r2 /= 2;
     int x = -r1;
@@ -71,32 +89,37 @@ void BonsaiCanvas::DrawAngledPointEllipse(int x1, int y1, int r1, int r2,
 
     double max_angle_rad = max_angle_deg * M_PI / 180.0;
 
-    auto draw_if_within_angle = [&](int dx, int dy) {
+    auto draw_if_within_angle = [&](int dx, int dy)
+    {
         double angle = atan2(dy * r1, dx * r2);
         if (angle < 0)
-        angle += 2 * M_PI;
+            angle += 2 * M_PI;
         if (angle <= max_angle_rad)
-        this->DrawPoint(x1 + dx, 2 * (y1 + dy), true, s);
+            this->DrawPoint(x1 + dx, 2 * (y1 + dy), true, s);
     };
 
-    do {
+    do
+    {
         draw_if_within_angle(-x, y);
         draw_if_within_angle(x, y);
         draw_if_within_angle(x, -y);
         draw_if_within_angle(-x, -y);
 
         e2 = 2 * err;
-        if (e2 >= dx) {
-        x++;
-        err += dx += 2 * r2 * r2;
+        if (e2 >= dx)
+        {
+            x++;
+            err += dx += 2 * r2 * r2;
         }
-        if (e2 <= dy) {
-        y++;
-        err += dy += 2 * r1 * r1;
+        if (e2 <= dy)
+        {
+            y++;
+            err += dy += 2 * r1 * r1;
         }
     } while (x <= 0);
 
-    while (y++ < r2) {
+    while (y++ < r2)
+    {
         draw_if_within_angle(0, y);
         draw_if_within_angle(0, -y);
     }
--- widgets/canvas_ext.hpp
+++ widgets/canvas_ext.hpp
@@ -9,11 +9,17 @@ public:
     void DrawAngledBlockEllipse(int x1, int y1, int r1, int r2,
                                 double max_angle_deg);
 
+    void DrawAngledBlockEllipse(int x, int y, int r1, int r2,
+                                const ftxui::Color& color, double max_angle_deg);
+
     void DrawAngledBlockEllipse(int x1, int y1, int r1, int r2,
                                 const Stylizer& s, double max_angle_deg);
 
     void DrawAngledPointEllipse(int x1, int y1, int r1, int r2,
                                 double max_angle_deg);
+    
+    void DrawAngledPointEllipse(int x, int y, int r1, int r2,
+                                const ftxui::Color& color, double max_angle_deg);
 
     void DrawAngledPointEllipse(int x1, int y1, int r1, int r2,
                                 const Stylizer& s, double max_angle_deg);
