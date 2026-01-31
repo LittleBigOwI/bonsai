31be8d61a42b8b654845eb2e50f5f308c7b41b8d
LittleBigOwI <syl20.row@gmail.com>
2026-01-31 10:37:49 +0100

    Rewrite: fix back entry size

--- src/ui/menu.cpp
+++ src/ui/menu.cpp
@@ -109,7 +109,7 @@ Component BonsaiMenu::menu(ScreenInteractive* screen, std::shared_ptr<BonsaiMenu
         auto row = hbox({
             text(" " + icon_str + " " + item.label) | size(WIDTH, LESS_THAN, Config::get().SIDEBAR_WIDTH - 15),
             filler(),
-            text(FormatUtils::toReadableShort(item.size)) | color(Color::GrayDark)
+            text(item.label == ".." ? "" : FormatUtils::toReadableShort(item.size)) | color(Color::GrayDark)
         });
 
         return is_selected ? row | bgcolor(Color::White) | color(Color::Black)
